c311085b34d719d5b7308d63dd4ed5192ce946e4
Yire Morlans <yire.m@icloud.com>
2023-04-19 15:18:20 -0400

    refactor: made reusable component for use with bugs and sea creature API data

--- src/components/CritterCard.jsx
+++ src/components/CritterCard.jsx
@@ -1,9 +1,10 @@
-const CritterCard = ({hemisphere, northFish, southFish}) => {
+const CritterCard = ({critterType, hemisphere, northFauna, southFauna}) => {
     return (        
         <div className="daily-critter">      
-            <h4>Fish</h4>
+            <h4>{critterType}</h4>
             <ul>
-            {hemisphere ? northFish.map((fish, index) => <li key={index}><img src={fish.image_url} alt={fish.name} /></li>) : southFish.map((fish, index) => <li key={index}><img src={fish.image_url} alt={fish.name} /></li>)}
+                {hemisphere ? northFauna.map((critter, index) => <li key={index}><img src={critter.image_url} alt={critter.name} /></li>) : 
+                southFauna.map((critter, index) => <li key={index}><img src={critter.image_url} alt={critter.name} /></li>)}
             </ul>
         </div>
     )
